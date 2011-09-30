705f676188fde1192534a99cdcffa9634b8ede41
Jim Barritt <jim@planet-ix.com>
2011-09-30 13:36:33 +0100

    [jmdb] Adding a test for represenation decoding

---
+++ http/src/test/unit/java/ixcode/platform/http/representation/RepresentationDecoderTest.java
@@ -0,0 +1,28 @@
+package ixcode.platform.http.representation;
+
+import ixcode.platform.http.server.*;
+import org.junit.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class RepresentationDecoderTest {
+
+    @Test
+    public void decodes_the_links() throws Exception {
+
+        String xml = "<someRepresentation><link title=\"foobar\" rel=\"alternate\" href=\"http://foobar.com\" /></someRepresentation>";
+
+        RepresentationDecoder decoder = new RepresentationDecoder(SomeObject.class);
+
+
+        Representation representation = decoder.representationFrom(xml);
+
+        assertThat(representation.getHyperlinksMatching("alternate", "foobar").uri.toURL().toExternalForm(), is("http://foobar.com"));
+
+    }
+
+    private static class SomeObject {
+
+    }
+}
