023f491f8a1898daf748d746475bbdcfd07d635d
Jim Barritt <jim@planet-ix.com>
2011-10-03 10:26:41 +0100

    [jmdb] Adding jetty sources and allowing a static root dir to be set

--- http/src/main/java/ixcode/platform/http/server/HttpServer.java
+++ http/src/main/java/ixcode/platform/http/server/HttpServer.java
@@ -1,37 +1,35 @@
 package ixcode.platform.http.server;
 
+import ixcode.platform.io.*;
+import ixcode.platform.reflect.*;
 import org.apache.log4j.*;
 import org.eclipse.jetty.http.security.*;
 import org.eclipse.jetty.security.*;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.*;
 import org.eclipse.jetty.servlet.*;
-import ixcode.platform.io.*;
-import ixcode.platform.reflect.*;
 
 import javax.servlet.*;
-
 import java.io.*;
 
+import static ixcode.platform.logging.ConsoleLog4jLogging.*;
 import static java.lang.String.*;
-import static ixcode.platform.logging.ConsoleLog4jLogging.initialiseLog4j;
-import static java.lang.String.format;
 
 public class HttpServer {
     private static final Logger LOG = Logger.getLogger(HttpServer.class);
 
     private Server server;
-    private final String webRoot;
     private final String contextPath;
     private Servlet rootServlet;
     private String serverName;
     private int httpPort;
+    private String webrootDir;
 
     public HttpServer(String serverName, int port, ixcode.platform.http.RequestDispatcher rootServlet) {
         this(serverName, "web", port, "/", rootServlet);
     }
 
-    public HttpServer(String serverName, String webRootDir,
+    public HttpServer(String serverName, String webrootDir,
                       int httpPort,
                       String contextPath,
                       Servlet rootServlet) {
@@ -39,19 +37,19 @@ public class HttpServer {
 
         this.serverName = serverName;
         this.httpPort = httpPort;
-        this.webRoot = webRootDir;
+        this.webrootDir = webrootDir;
         this.contextPath = contextPath;
         this.rootServlet = rootServlet;
-
-        server = new Server(httpPort);
-        server.setHandler(handlers());
     }
 
-
+    public HttpServer servingStaticContentFrom(String webrootDir) {
+        this.webrootDir = webrootDir;
+        return this;
+    }
 
     public static void main(String args[]) {
         initialiseLog4j();
-        new HttpServer(args[0], "web", 8080, "/",  loadServletClass(args[1])).start();
+        new HttpServer(args[0], "web", 8080, "/", loadServletClass(args[1])).start();
     }
 
     private static Servlet loadServletClass(String servletClassName) {
@@ -62,15 +60,16 @@ public class HttpServer {
     public void start() {
         try {
             LOG.info(format("Starting Http Server [%s] on port [%d]...", serverName, httpPort));
+            server = new Server(httpPort);
+            server.setHandler(handlers());
             server.start();
             new SystemProcess().writeProcessIdToFile(".webserver.pid");
-            LOG.info(format("Http Server Started. Serving using the dispatcher [%s]", rootServlet.getClass().getName()));
+            LOG.info(format("Http Server Started. Serving using the dispatcher [%s], static content is from [%s]", rootServlet.getClass().getName(), new File(webrootDir).getCanonicalPath()));
             server.join();
         } catch (Exception e) {
             throw new HttpServerStartupException(e);
         }
     }
-
 
 
     private HandlerList handlers() {
@@ -81,7 +80,7 @@ public class HttpServer {
 
     private ResourceHandler resourceHandler() {
         ResourceHandler resourceHandler = new ResourceHandler();
-        resourceHandler.setResourceBase(webRoot);
+        resourceHandler.setResourceBase(webrootDir);
         return resourceHandler;
     }
 
@@ -89,7 +88,7 @@ public class HttpServer {
         ServletContextHandler servletHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
         servletHandler.setContextPath(contextPath);
-        servletHandler.setResourceBase(webRoot);
+        servletHandler.setResourceBase(webrootDir);
         ErrorHandler errorHandler = new ErrorHandler();
         errorHandler.setServer(server);
         servletHandler.setErrorHandler(errorHandler);
@@ -98,12 +97,10 @@ public class HttpServer {
     }
 
 
-
     private static ConstraintMapping mapConstraintTo(Constraint constraint, String path) {
         ConstraintMapping cm = new ConstraintMapping();
         cm.setPathSpec(path);
         cm.setConstraint(constraint);
         return cm;
     }
-
 }
