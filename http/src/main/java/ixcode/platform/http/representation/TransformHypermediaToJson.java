a06bb9345f507461b9c6d0ed6836f44b7e3b92bd
Jim Barritt <jim@planet-ix.com>
2011-10-17 22:44:30 +0100

    [jmdb] Got simple list of URI's working

--- http/src/main/java/ixcode/platform/http/representation/TransformHypermediaToJson.java
+++ http/src/main/java/ixcode/platform/http/representation/TransformHypermediaToJson.java
@@ -1,8 +1,10 @@
 package ixcode.platform.http.representation;
 
-import ixcode.platform.json.JsonObject;
 import ixcode.platform.serialise.TransformToJson;
 import ixcode.platform.text.format.UriFormat;
+
+import java.net.URI;
+import java.net.URL;
 
 import static ixcode.platform.json.JsonObjectBuilder.jsonObjectWith;
 
@@ -16,7 +18,15 @@ public class TransformHypermediaToJson extends TransformToJson {
             if (hyperlink.relation == null) {
                 return uriFormat.format(hyperlink.uri);
             }
-            return jsonObjectWith().key(hyperlink.relation).value(uriFormat.format(hyperlink.uri));
+            return jsonObjectWith().key(hyperlink.relation).value(uriFormat.format(hyperlink.uri)).build();
+        } else if (value instanceof URI) {
+            return uriFormat.format((URI)value);
+        }  else if (value instanceof URL) {
+            try {
+                return uriFormat.format(((URL)value).toURI());
+            } catch (Exception e) {
+                throw new RuntimeException("Couldn not format URL: " + value + " (See Cause)", e);
+            }
         }
         return super.jsonValueOf(value);
     }
