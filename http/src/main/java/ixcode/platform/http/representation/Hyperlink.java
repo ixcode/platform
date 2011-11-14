1b0f6b1c28561c104b291d8b8e99a39a48b1b8c7
Jim Barritt <jim@planet-ix.com>
2011-11-14 20:54:57 +0000

    [jmdb] Doing something with hyperlinks

--- http/src/main/java/ixcode/platform/http/representation/Hyperlink.java
+++ http/src/main/java/ixcode/platform/http/representation/Hyperlink.java
@@ -27,4 +27,21 @@ public class Hyperlink {
     public boolean hasTitle() {
         return title != null;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Hyperlink hyperlink = (Hyperlink) o;
+
+        if (uri != null ? !uri.equals(hyperlink.uri) : hyperlink.uri != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return uri != null ? uri.hashCode() : 0;
+    }
 }
