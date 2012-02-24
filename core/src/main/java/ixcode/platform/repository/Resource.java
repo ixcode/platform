3fb7ebfc2dc21b1c89952f87c0f3d91067ddac10
Jim Barritt <jim@planet-ix.com>
2012-02-24 17:22:28 +0000

    [jmdb] Adding google reflections

---
+++ core/src/main/java/ixcode/platform/repository/Resource.java
@@ -0,0 +1,8 @@
+package ixcode.platform.repository;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Resource {
+}
