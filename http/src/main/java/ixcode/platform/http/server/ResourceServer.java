227c9b59d43a761823e059ce9a9914e6558b4a02
Jim Barritt <jim@planet-ix.com>
2012-03-05 17:32:33 +0000

    [jmdb] Moving everything into the framework

--- http/src/main/java/ixcode/platform/http/server/ResourceServer.java
+++ http/src/main/java/ixcode/platform/http/server/ResourceServer.java
@@ -2,8 +2,12 @@ package ixcode.platform.http.server;
 
 import ixcode.platform.http.server.redirection.RedirectTrailingSlashes;
 import ixcode.platform.http.server.resource.RouteMap;
+import ixcode.platform.repository.Repository;
+
+import java.util.Map;
 
 import static ixcode.platform.http.server.RequestDispatcher.requestDispatcher;
+import static ixcode.platform.http.server.ResourceServerContext.reflectivelyBuildRepositoryMap;
 import static ixcode.platform.logging.ConsoleLog4jLogging.initialiseLog4j;
 import static java.lang.String.format;
 
@@ -12,12 +16,18 @@ public class ResourceServer {
     private final String hostname;
     private final int port;
     private final RouteMap routeMap;
+    private ResourceServerContext context;
 
 
-    public ResourceServer(String hostname, int port, ResourceServerContext resourceServerContext) {
+    public ResourceServer(String hostname, int port, String rootResourcePackageName) {
+        this(hostname, port, new ResourceServerContext(reflectivelyBuildRepositoryMap(rootResourcePackageName)));
+    }
+    
+    private ResourceServer(String hostname, int port, ResourceServerContext resourceServerContext) {
+        this.context = resourceServerContext;
         this.hostname = hostname;
         this.port = port;
-        this.routeMap = resourceServerContext.theRouteMap().rootedAt(format("http://%s:%s", hostname, port));
+        this.routeMap = context.theRouteMap().rootedAt(format("http://%s:%s", hostname, port));
         initialiseLog4j();
     }
 
@@ -32,4 +42,7 @@ public class ResourceServer {
     }
 
 
+    public ResourceServerContext getContext() {
+        return context;
+    }
 }
--- http/src/main/java/ixcode/platform/http/server/ResourceServerContext.java
+++ http/src/main/java/ixcode/platform/http/server/ResourceServerContext.java
@@ -1,21 +1,34 @@
 package ixcode.platform.http.server;
 
+import ixcode.platform.collection.MapBuilder;
 import ixcode.platform.di.InjectionContext;
 import ixcode.platform.http.server.resource.RouteMap;
 import ixcode.platform.http.server.routing.ResourceRoute;
+import ixcode.platform.repository.Repository;
+import ixcode.platform.repository.Resource;
+import org.reflections.Reflections;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import static ixcode.platform.collection.MapBuilder.linkedHashMapWith;
 import static ixcode.platform.http.server.resource.RouteMap.aResourceMap;
+import static ixcode.platform.repository.LinkedHashMapRepository.createRepositoryFor;
 
 public class ResourceServerContext {
 
     private final InjectionContext injectionContext = new InjectionContext();
+    private final Map<String, Repository<?>> repositoryMap;
 
 
-    public ResourceServerContext(Map repositoryMap) {
-        RouteMap hyperRouteMap = registerRouteMap(injectionContext, repositoryMap);
-
+    public ResourceServerContext(Map<String, Repository<?>> repositoryMap) {
+        this.repositoryMap = repositoryMap;
+        registerRouteMap(injectionContext, this.repositoryMap);
     }
 
     private static RouteMap registerRouteMap(InjectionContext injectionContext, Map repositoryMap) {
@@ -30,8 +43,37 @@ public class ResourceServerContext {
         injectionContext.register(hyperRouteMap); return hyperRouteMap;
     }
 
+    public static Map<String, Repository<?>> reflectivelyBuildRepositoryMap(String rootDomainPackage) {
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                                                          .setUrls(ClasspathHelper.forPackage(rootDomainPackage))
+                                                          .setScanners(new TypeAnnotationsScanner()));
+
+
+        Set<Class<?>> resourceTypes = reflections.getTypesAnnotatedWith(Resource.class);
+
+        List<Repository<?>> repositories = new ArrayList<Repository<?>>();
+        for (Class<?> resourceType : resourceTypes) {
+            repositories.add(createRepositoryFor(resourceType));
+        }
+
+        return buildRepositoryMapFrom(repositories);
+    }
+
+    public static Map buildRepositoryMapFrom(List<Repository<?>> repositories) {
+        MapBuilder repositoryMapBuilder = linkedHashMapWith();
+        for (Repository<?> repository : repositories) {
+            repositoryMapBuilder.key(repository.getRepositoryId()).value(repository);
+        }
+
+        return repositoryMapBuilder.build();
+    }
+
 
     public RouteMap theRouteMap() {
         return injectionContext.getThe(RouteMap.class);
+    }
+
+    public Map<String, Repository<?>> theRepositoryMap() {
+        return repositoryMap;
     }
 }
