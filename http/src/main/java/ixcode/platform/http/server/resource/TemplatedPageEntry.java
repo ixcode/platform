8928cb32b9afc7ee4c336eb33f9417aea301e0dd
Jim Barritt <jim@planet-ix.com>
2012-07-06 15:26:15 +0100

    [jmdb] Added template parameters

--- http/src/main/java/ixcode/platform/http/server/resource/TemplatedPageEntry.java
+++ http/src/main/java/ixcode/platform/http/server/resource/TemplatedPageEntry.java
@@ -1,19 +1,28 @@
 package ixcode.platform.http.server.resource;
 
+import ixcode.platform.collection.MapBuilder;
 import ixcode.platform.http.protocol.HttpMethod;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TemplatedPageEntry {
-    public String path = "/subsection/some-page";
-    public HttpMethod[] methods = {HttpMethod.GET, HttpMethod.POST};
-    public String templateName = "subsection/some-page";
-    public Map<String, Object> data = new HashMap<String, Object>();
-    public URI redirectUri;
-    public List<DataProvider> dataProviders;
+import static ixcode.platform.collection.MapBuilder.linkedHashMapWith;
 
-    public List<DataConsumer> dataConsumers;
+public class TemplatedPageEntry {
+    public final String path = "/subsection/some-page";
+    public final HttpMethod[] methods = {HttpMethod.GET, HttpMethod.POST};
+    public final String templateName = "subsection/some-page";
+
+    public final Map<String, Object> data = linkedHashMapWith()
+            .key("title").value("A Dynamic page")
+            .key("message").value("Seek and Ye shall find")
+            .build();
+
+    public final URI redirectUri = null;
+    public final List<DataProvider> dataProviders = new ArrayList<DataProvider>();
+    public final List<DataConsumer> dataConsumers = new ArrayList<DataConsumer>();
 }
