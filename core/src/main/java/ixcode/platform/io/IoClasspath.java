a2e0d83fd26edd4953317d1cc587ed28175e01a2
Jim Barritt <jim@planet-ix.com>
2012-10-05 09:55:01 +0100

    Added extra methods to loading things from the classpath so we can load relative to a class.

--- core/src/main/java/ixcode/platform/io/IoClasspath.java
+++ core/src/main/java/ixcode/platform/io/IoClasspath.java
@@ -9,5 +9,12 @@ public class IoClasspath {
     public static InputStream inputStreamFromClasspathEntry(String classpathEntry) {
         return currentThread().getContextClassLoader().getResourceAsStream(classpathEntry);
     }
+
+    public static InputStream inputStreamFromClasspathEntry(Object location, String classpathEntry) {
+        return inputStreamFromClasspathEntry(location.getClass(), classpathEntry);
+    }
+    public static InputStream inputStreamFromClasspathEntry(Class<?> location, String classpathEntry) {
+        return location.getClassLoader().getResourceAsStream(classpathEntry);
+    }
     
 }
